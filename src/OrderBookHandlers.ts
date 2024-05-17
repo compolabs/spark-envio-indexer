3db8bb64a1e702443186e37772d5c61c8159bba8
Dmitry Zakharov <dzakh.dev@gmail.com>
2024-05-17 15:06:34 +0400

    Handle OrderChangeEvent

--- src/OrderBookHandlers.ts
+++ src/OrderBookHandlers.ts
@@ -1,4 +1,4 @@
-import { OrderBookContract } from "generated";
+import { OrderBookContract, spotOrderEntity } from "generated";
 import { nanoid } from "nanoid";
 
 function tai64ToDate(tai64nBigInt: bigint) {
@@ -15,6 +15,13 @@ function tai64ToDate(tai64nBigInt: bigint) {
   return new Date(Number(milliseconds)).toISOString();
 }
 
+function decodeI64(i64: {
+  readonly value: bigint;
+  readonly negative: boolean;
+}) {
+  return (i64.negative ? "-" : "") + i64.value.toString();
+}
+
 OrderBookContract.MarketCreateEvent.loader(({ event, context }) => {});
 
 OrderBookContract.MarketCreateEvent.handler(({ event, context }) => {
@@ -24,4 +31,31 @@ OrderBookContract.MarketCreateEvent.handler(({ event, context }) => {
     asset_id: event.data.asset_id.value,
     timestamp: tai64ToDate(event.data.timestamp),
   });
+});
+
+OrderBookContract.OrderChangeEvent.loader(({ event, context }) => {});
+
+OrderBookContract.OrderChangeEvent.handler(({ event, context }) => {
+  const eventOrder = event.data.order;
+  const timestamp = tai64ToDate(event.data.timestamp);
+  const order: spotOrderEntity | null = eventOrder
+    ? {
+        id: eventOrder.id,
+        trader: eventOrder.trader.value,
+        base_token: eventOrder.base_token.value,
+        base_size: decodeI64(eventOrder.base_size),
+        base_price: eventOrder.base_price,
+        timestamp,
+      }
+    : null;
+
+  context.SpotOrderChangeEvent.set({
+    id: nanoid(),
+    order_id: event.data.order_id,
+    new_base_size: order ? order.base_size : "0",
+    timestamp,
+  });
+  if (order) {
+    context.SpotOrder.set(order);
+  }
 });
