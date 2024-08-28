60a63ac793986d6ce33bc9b405cfabc0f908322c
Paul <zhpv96@gmail.com>
2024-08-28 13:40:52 +0300

    delete withdraw and deposit

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,6 +1,4 @@
-export * from "./withdrawEventHandler";
 export * from "./openOrderEventHandler";
 export * from "./cancelOrderEventHandler";
-export * from "./depositEventHandler";
 export * from "./tradeOrderEventHandler";
 export * from "./matchOrderEventHandler";
