834827df7d3dbd35453f6b115a5d87104bee44a7
Paul <zhpv96@gmail.com>
2024-09-05 22:01:49 +0300

    open order done

--- src/loaders/openOrderEventLoader.ts
+++ src/loaders/openOrderEventLoader.ts
@@ -3,7 +3,8 @@ import {
   OrderBookContract_OpenOrderEventEvent_loaderContext,
 } from "generated";
 import { handlerArgs } from "generated/src/Handlers.gen";
-import crypto from "crypto";
+import { getHash } from "../utils/getHash";
+import { BASE_ASSET, QUOTE_ASSET } from "../utils/marketConfig";
 
 export const openOrderEventLoader = ({
   event,
@@ -13,4 +14,14 @@ export const openOrderEventLoader = ({
   OrderBookContract_OpenOrderEventEvent_loaderContext
 >) => {
   context.Order.load(event.data.order_id);
+
+  const orderType = event.data.order_type.case;
+
+  if (orderType === "Buy") {
+    const balanceId = getHash(`${QUOTE_ASSET}-${event.data.user.payload.bits}`);
+    context.Balance.load(balanceId);
+  } else if (orderType === "Sell") {
+    const balanceId = getHash(`${BASE_ASSET}-${event.data.user.payload.bits}`);
+    context.Balance.load(balanceId);
+  }
 };
