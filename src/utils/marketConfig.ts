0e989bc0ca0a58dc197e7f52fac6cc8433fbeaa0
nazgull08 <nazgull08@protonmail.com>
2024-09-10 13:31:50 +0300

    eth-usdc market

--- src/utils/marketConfig.ts
+++ src/utils/marketConfig.ts
@@ -1,5 +1,5 @@
-export const BASE_ASSET = "0x38e4ca985b22625fff93205e997bfc5cc8453a953da638ad297ca60a9f2600bc";
+export const BASE_ASSET = "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07";
 export const QUOTE_ASSET = "0x336b7c06352a4b736ff6f688ba6885788b3df16e136e95310ade51aa32dc6f05";
-export const BASE_DECIMAL = 10n ** 8n;
+export const BASE_DECIMAL = 10n ** 9n;
 export const QUOTE_DECIMAL = 10n ** 6n; 
 export const PRICE_DECIMAL = 10n ** 9n; 
